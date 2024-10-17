9ff5343ca22177b3ee9428d9391883d5b1b005ce
alonso.fritz <alonso.fritz@sovis.com.br>
2024-10-17 17:40:50 -0300

    first commit

---
+++ src/main/java/com/alonsofritz/simplified_authenticator_spring/SimplifiedAuthenticatorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.alonsofritz.simplified_authenticator_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedAuthenticatorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedAuthenticatorSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alonsofritz/simplified_authenticator_spring/SimplifiedAuthenticatorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alonsofritz.simplified_authenticator_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedAuthenticatorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
